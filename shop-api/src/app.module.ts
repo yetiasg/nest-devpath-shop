6015214344c54d7577725c7d5c94d589e804d413
cherryvue <yetiasg@gmail.com>
2022-01-16 22:09:06 +0100

    Add new modules

--- shop-api/src/app.module.ts
+++ shop-api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [UsersModule, AdminModule, AuthModule, CategoriesModule, MailModule],
 })
 export class AppModule {}
