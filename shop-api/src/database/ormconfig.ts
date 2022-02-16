802a655da1f37b4587b5f0830603a52531bde3ba
Mateusz Żupa <yetiasg@gmail.com>
2022-02-16 14:45:01 +0100

    Create  migration

--- shop-api/src/database/ormconfig.ts
+++ shop-api/src/database/ormconfig.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: '../../development.env' });
 
-module.exports = {
+export default {
   type: 'postgres',
   host: process.env.TYPEORM_HOST,
   port: Number(process.env.TYPEORM_PORT ?? 5433),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
+  username: 'postgres',
+  password: 'postgres',
   database: process.env.TYPEORM_DATABASE,
   entities: ['./**/*.entity.{js,ts}'],
   migrations: [__dirname + '/../database/migrations/*.{js,ts}'],
