ec5974f77db99cd630ab5be963f7015f6e7cec82
Mateusz Żupa <yetiasg@gmail.com>
2022-02-17 08:44:15 +0100

    Update orm config, generate new migration

---
+++ shop-api/src/database/migrations/1645083770172-mig-02.ts
@@ -0,0 +1,38 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class mig021645083770172 implements MigrationInterface {
+    name = 'mig021645083770172'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "Category" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" text NOT NULL, "description" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_c2727780c5b9b0c564c29a4977c" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "Product" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "price" numeric NOT NULL, "description" text NOT NULL, "stock" integer NOT NULL, "archived" boolean NOT NULL DEFAULT false, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_9fc040db7872192bbc26c515710" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "public"."User_role_enum" AS ENUM('ADMIN', 'USER')`);
+        await queryRunner.query(`CREATE TABLE "User" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "active" boolean NOT NULL DEFAULT false, "email" character varying NOT NULL, "firstName" character varying, "lastName" character varying, "address" json, "activationToken" character varying NOT NULL DEFAULT '', "resetPasswordToken" character varying NOT NULL DEFAULT '', "password" character varying NOT NULL, "role" "public"."User_role_enum" NOT NULL DEFAULT 'USER', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_4a257d2c9837248d70640b3e36e" UNIQUE ("email"), CONSTRAINT "PK_9862f679340fb2388436a5ab3e4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "Order" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" character varying NOT NULL, "totalPrice" numeric NOT NULL, "status" character varying NOT NULL DEFAULT 'NEW', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userIdId" uuid, CONSTRAINT "PK_3d5a3861d8f9a6db372b2b317b7" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "OrderItems" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "orderId" character varying NOT NULL, "productId" character varying NOT NULL, "amount" integer NOT NULL, "price" numeric NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "ordersId" uuid, CONSTRAINT "PK_567f75d7ff079b9ab3e6dd33708" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "product_categories_category" ("productId" uuid NOT NULL, "categoryId" uuid NOT NULL, CONSTRAINT "PK_17f2a361443184000ee8d79f240" PRIMARY KEY ("productId", "categoryId"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_342d06dd0583aafc156e076379" ON "product_categories_category" ("productId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_15520e638eb4c46c4fb2c61c4b" ON "product_categories_category" ("categoryId") `);
+        await queryRunner.query(`ALTER TABLE "Order" ADD CONSTRAINT "FK_c4fdb5e41c8f17d104e7a661519" FOREIGN KEY ("userIdId") REFERENCES "User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "OrderItems" ADD CONSTRAINT "FK_274cb1921d2402c94c1ec925b5c" FOREIGN KEY ("ordersId") REFERENCES "Order"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "product_categories_category" ADD CONSTRAINT "FK_342d06dd0583aafc156e0763790" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE "product_categories_category" ADD CONSTRAINT "FK_15520e638eb4c46c4fb2c61c4b4" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "product_categories_category" DROP CONSTRAINT "FK_15520e638eb4c46c4fb2c61c4b4"`);
+        await queryRunner.query(`ALTER TABLE "product_categories_category" DROP CONSTRAINT "FK_342d06dd0583aafc156e0763790"`);
+        await queryRunner.query(`ALTER TABLE "OrderItems" DROP CONSTRAINT "FK_274cb1921d2402c94c1ec925b5c"`);
+        await queryRunner.query(`ALTER TABLE "Order" DROP CONSTRAINT "FK_c4fdb5e41c8f17d104e7a661519"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_15520e638eb4c46c4fb2c61c4b"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_342d06dd0583aafc156e076379"`);
+        await queryRunner.query(`DROP TABLE "product_categories_category"`);
+        await queryRunner.query(`DROP TABLE "OrderItems"`);
+        await queryRunner.query(`DROP TABLE "Order"`);
+        await queryRunner.query(`DROP TABLE "User"`);
+        await queryRunner.query(`DROP TYPE "public"."User_role_enum"`);
+        await queryRunner.query(`DROP TABLE "Product"`);
+        await queryRunner.query(`DROP TABLE "Category"`);
+    }
+
+}
