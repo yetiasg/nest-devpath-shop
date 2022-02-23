2f93092e6480d880df8479040be99d84c1e994c4
Mateusz Żupa <yetiasg@gmail.com>
2022-02-23 12:36:53 +0100

    Clearing cart after creating an order

--- shop-api/src/orders/orders.controller.ts
+++ shop-api/src/orders/orders.controller.ts
@@ -45,6 +45,7 @@ export class OrdersController {
   @Serialize(OrderEntity)
   @UseGuards(RoleGuard([Role.ADMIN, Role.USER]))
   async createOrder(@CurrentUserId() id: string, @Body('items') items) {
+    console.log(items);
     return await this.ordersService.createOrder(id, items);
   }
 
